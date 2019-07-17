8afd4d9ade9d28dd4cfa28084b511b7d1b0ee19d
dfSabogal10 <david.sabogal1@gmail.com>
2019-07-16 22:54:09 -0500

    servicio rest cliente

--- src/main/java/com/example/ClientesController.java
+++ src/main/java/com/example/ClientesController.java
@@ -3,17 +3,12 @@ package com.example;
 import java.net.URI;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicLong;
-
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 @RestController
 public class ClientesController {
@@ -52,6 +45,29 @@ public class ClientesController {
 		      }
 
 		      return output;
+		    } catch (Exception e) {
+		      return null;
+		    }
+    }
+	
+	@RequestMapping(value="/cliente", method=RequestMethod.GET)
+	public Cliente cliente(@RequestParam(value="id") int id) {
+		try (Connection connection = dataSource.getConnection()) {
+		      Statement stmt = connection.createStatement();
+		      ResultSet rs = stmt.executeQuery("SELECT * FROM clientes WHERE id="+id+";");
+
+		      if(rs.next()) {
+		    	 Cliente nuevo=new Cliente(rs.getInt("id"),
+		    			 					rs.getString("apellido"),
+		    			 					rs.getString("nombre"),
+		    			 					rs.getInt("cedula"),
+		    			 					rs.getLong("tarjeta"),
+		    			 					rs.getDate("fechaVencimiento"));
+		        return nuevo;
+		      }
+		      else{
+		    	  return null;
+		      }
 		    } catch (Exception e) {
 		      return null;
 		    }
